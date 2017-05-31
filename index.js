e2099a6dfbcc8224134d7c6c87bb2b371bf5fd89
Andrew McGill <arm5077@gmail.com>
2017-05-31 08:17:52 -0400

    Add beginning of API and fix date format bug

--- index.js
+++ index.js
@@ -1,155 +1,65 @@
-require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient,
-  cheerio = require('cheerio'),
-  async = require('async'),
-  request = require('request'),
-  moment = require('moment-timezone');
-  
-var $ = null, 
-  db = null;
-  
-var positions = [
-  {
-    name: 'lead',
-    selector: 'a.c-hp-lead__link'
-  },
-  {
-    name: 'filmstrip',
-    selector: 'h3 a.c-hp-filmstrip__link'
-  },
-  {
-    name: 'offlead',
-    selector: '.c-hp-offlead__link--hed'
-  },
-  {
-    name: 'featured',
-    selector: '.c-hp-featured__link'
-  },
-  {
-    name: 'news',
-    selector: '.c-hp-news__link'
-  }
-]  
+require('dotenv').config()
+
+const express = require('express'),
+  moment = require('moment-timezone'),
+  MongoClient = require('mongodb').MongoClient;
+
+app = express();
 
 // Connect to Mongo database
-var client = MongoClient.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/homepageTracker')
-  .then(function(database){
-    db = database;
-    return db.collection('positions')
-  })
-
-// Begin main waterfall loop
-async.series([
-  
-  // Grab the Atlantic's homepage
-  function(nextStep){
-    request.get('https://theatlantic.com', function(err, res, body){
-      if( err ) throw err;
-      $ = cheerio.load(body)
-      return nextStep();
-      
+var db = null
+MongoClient.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/homepageTracker')
+  .then(function(result){
+    db = result
+   
+    db.collection('positions').find().forEach(function(element){
+      element.stories.forEach(function(story, i){
+        element.stories[i].newStart = moment(story.start).tz('America/New_York').toDate()
+        element.stories[i].newEnd = moment(story.end).tz('America/New_York').toDate()
+      })
+      console.log(element)
+      db.collection('positions').save(element)
     })
-  },
-  
-  // Loop through areas of interest and query against Mongo
-  function(nextStep){
-    async.each(positions, function(position, nextPosition){
-      
-      // Some of these positions have different spots, so we need to loop through them too
-      async.eachOf( $(position.selector), function(subposition, i, nextSubposition){
 
-        // Query database for latest headline attached to position);
-        client.then(function(collection){
-            return collection.aggregate(
-            [
-              {
-                $match: { name: position.name, slot: i+1 },
-              },
-              {
-                $project: { 
-                  name: 1,
-                  start: 1,
-                  lastTitle: { $arrayElemAt: [ '$stories', -1 ]}
-      
-                }
+  });
+
+app.get('/', function(req, res){ 
+
+  console.log(moment().tz("America/New_York").startOf('day').format());
+
+  db.collection('positions').aggregate(
+    [
+      {
+        $match: {},
+      },
+      {
+        $project: { 
+          name: true,
+          slot: true,
+          stories: { 
+            $filter: {
+              input: '$stories',
+              as: "story",
+              cond: { 
+                $and: [
+                  { $gte: [ '$$story.newStart', moment().tz("America/New_York").startOf('day').toDate() ] }/*,
+                  { $lte: [ '$$story.end', moment().tz("America/New_York").endOf('day').format() ] }*/
+                ]                
               }
-            ]
-          ).toArray();
-        })
-        .then(function(match){
-          // If no results, insert new record
-          // (This really should only happen once)
-          if( match.length == 0){
-            client.then(function(collection){
-              collection.insert({ 
-                'name': position.name,
-                'slot': i+1,
-                'stories': [
-                  {
-                    title: $(subposition).text().trim(),
-                    url: $(subposition).attr('href'),
-                    start: moment().tz('America/New_York').format()
-                  }
-                ]
-              }, function(err, result){
-                if(err) throw err;
-                console.log(`Inserted new '${position.name}' object...`)
-                return nextSubposition()
-              })
-            })
+            }
           }
-          else {
-            // OK, this position already exists... let's see if the last headline recorded matches what's still on the site
-            var match = match[0]
-            // If they match, let's continue
-            if(match.lastTitle.url == $(subposition).attr('href'))
-              return nextSubposition()
 
+        }
+      }
+    ]
+  ).toArray()
+  .then(function(result){
+    res.json(result)
+  });
 
-            // First, update the current headline and add an "end"
-            return client.then(function(collection){
-               collection.update(
-                { name: position.name, 
-                  slot: i+1, 
-                  'stories.url': match.lastTitle.url, 
-                  'stories.start': match.lastTitle.start 
-                },
-                { $set: { 'stories.$.end': moment().tz('America/New_York').format() } }
-              )
-            })
-            // Then let's add a new record for the new headline
-            .then(function(){
-              console.log('merp');
-              client.then(function(collection){
-                collection.update({
-                  'name': position.name,
-                  'slot': i+1
-                }, 
-                {
-                  $push: {
-                    stories: {
-                      title: $(subposition).text().trim(),
-                      url: $(subposition).attr('href'),
-                      start: moment().tz('America/New_York').format()
-                    }
-                  }
-                })
-              })
-            })
-            .then(function(){
-              console.log(`Added ${ $(subposition).text().trim() } to ${position.name}-${ i+1 }`)
-              return nextSubposition()
-            })      
-          }
-        })
-        .catch(function(err){
-          throw err;
-        })
-      }, nextPosition)
-    }, nextStep)
-  }
-], function(err){
-  if(err) throw err
-  console.log("I'm done!")
-  db.close();
+})
+
+app.listen(3000, function(err){
+  if( err ) throw err;
+  console.log("Listening!")
 })
---
+++ scrape.js
@@ -0,0 +1,155 @@
+require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient,
+  cheerio = require('cheerio'),
+  async = require('async'),
+  request = require('request'),
+  moment = require('moment-timezone');
+  
+var $ = null, 
+  db = null;
+  
+var positions = [
+  {
+    name: 'lead',
+    selector: 'a.c-hp-lead__link'
+  },
+  {
+    name: 'filmstrip',
+    selector: 'h3 a.c-hp-filmstrip__link'
+  },
+  {
+    name: 'offlead',
+    selector: '.c-hp-offlead__link--hed'
+  },
+  {
+    name: 'featured',
+    selector: '.c-hp-featured__link'
+  },
+  {
+    name: 'news',
+    selector: '.c-hp-news__link'
+  }
+]  
+
+// Connect to Mongo database
+var client = MongoClient.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/homepageTracker')
+  .then(function(database){
+    db = database;
+    return db.collection('positions')
+  })
+
+// Begin main waterfall loop
+async.series([
+  
+  // Grab the Atlantic's homepage
+  function(nextStep){
+    request.get('https://theatlantic.com', function(err, res, body){
+      if( err ) throw err;
+      $ = cheerio.load(body)
+      return nextStep();
+      
+    })
+  },
+  
+  // Loop through areas of interest and query against Mongo
+  function(nextStep){
+    async.each(positions, function(position, nextPosition){
+      
+      // Some of these positions have different spots, so we need to loop through them too
+      async.eachOf( $(position.selector), function(subposition, i, nextSubposition){
+
+        // Query database for latest headline attached to position);
+        client.then(function(collection){
+            return collection.aggregate(
+            [
+              {
+                $match: { name: position.name, slot: i+1 },
+              },
+              {
+                $project: { 
+                  name: 1,
+                  start: 1,
+                  lastTitle: { $arrayElemAt: [ '$stories', -1 ]}
+      
+                }
+              }
+            ]
+          ).toArray();
+        })
+        .then(function(match){
+          // If no results, insert new record
+          // (This really should only happen once)
+          if( match.length == 0){
+            client.then(function(collection){
+              collection.insert({ 
+                'name': position.name,
+                'slot': i+1,
+                'stories': [
+                  {
+                    title: $(subposition).text().trim(),
+                    url: $(subposition).attr('href'),
+                    start: moment().tz('America/New_York').toDate()
+                  }
+                ]
+              }, function(err, result){
+                if(err) throw err;
+                console.log(`Inserted new '${position.name}' object...`)
+                return nextSubposition()
+              })
+            })
+          }
+          else {
+            // OK, this position already exists... let's see if the last headline recorded matches what's still on the site
+            var match = match[0]
+            // If they match, let's continue
+            if(match.lastTitle.url == $(subposition).attr('href'))
+              return nextSubposition()
+
+
+            // First, update the current headline and add an "end"
+            return client.then(function(collection){
+               collection.update(
+                { name: position.name, 
+                  slot: i+1, 
+                  'stories.url': match.lastTitle.url, 
+                  'stories.start': match.lastTitle.start 
+                },
+                { $set: { 'stories.$.end': moment().tz('America/New_York').toDate() } }
+              )
+            })
+            // Then let's add a new record for the new headline
+            .then(function(){
+              console.log('merp');
+              client.then(function(collection){
+                collection.update({
+                  'name': position.name,
+                  'slot': i+1
+                }, 
+                {
+                  $push: {
+                    stories: {
+                      title: $(subposition).text().trim(),
+                      url: $(subposition).attr('href'),
+                      start: moment().tz('America/New_York').toDate()
+                    }
+                  }
+                })
+              })
+            })
+            .then(function(){
+              console.log(`Added ${ $(subposition).text().trim() } to ${position.name}-${ i+1 }`)
+              return nextSubposition()
+            })      
+          }
+        })
+        .catch(function(err){
+          throw err;
+        })
+      }, nextPosition)
+    }, nextStep)
+  }
+], function(err){
+  if(err) throw err
+  console.log("I'm done!")
+  db.close();
+})
